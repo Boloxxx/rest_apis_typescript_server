35d7c44be54175bec7ffbbc9d9310337a92f62d9
Boloxxx <bryanrodriguezalmonte@gmail.com>
2025-04-23 08:05:55 -0400

    change config db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,17 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false
+  models: [__dirname + '/../models/**/*'],
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default db;
